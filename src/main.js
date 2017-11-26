b501470684774544d3bf200be91662e87004483a
René Filip <renefilip@mail.com>
2017-11-26 22:07:31 +0900

    test full polyfill

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-// import "babel-polyfill";
+import "babel-polyfill";
 // require("core-js/fn/promise");
-import "core-js/fn/promise";
+// import "core-js/fn/promise";
 
 import * as file1 from './file1';
 import * as file2 from './file2';
